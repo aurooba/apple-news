4c27d50b309c097ff6d1d915f685c3a7cbe9c369
kevinfodness <kevinfodness@users.noreply.github.com>
2025-06-27 14:19:28 +0000

    Built changes from develop a8ce2f22cefd4325ae63fb08ef42051c4219bd5d

---
+++ build/pluginSidebar.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '87d5b79979f2524517be');
